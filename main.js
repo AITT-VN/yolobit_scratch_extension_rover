97ebc6c4991574c9a358a7f36a7fd98a98db7f33
truongphat230799 <taphat1753@gmail.com>
2023-06-11 12:15:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -164,15 +164,19 @@ class Scratch3YoloBitRover {
                     ],
                     arguments: {
                         SENSOR1: {
+                            type: ArgumentType.STRING,
                             menu: 'line_array'
                         },
                         SENSOR2: {
+                            type: ArgumentType.STRING,
                             menu: 'line_array'
                         },
                         SENSOR3: {
+                            type: ArgumentType.STRING,
                             menu: 'line_array'
                         },
                         SENSOR4: {
+                            type: ArgumentType.STRING,
                             menu: 'line_array'
                         }
                     },
